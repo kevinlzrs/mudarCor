53f1bd89a77077609fc7a5dad5c809c5c1ebf6ee
kevinlzrs <136590754+kevinlzrs@users.noreply.github.com>
2024-09-27 11:41:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function mudaCor(cor){
+    let body = document.body;
+    switch(cor){
+        case 'vermelho': body.style.backgroundColor = 'red'; break;
+        case 'azul': body.style.backgroundColor = 'blue'; break;
+        case 'verde': body.style.backgroundColor = 'green'; break;
+        case 'amarelo': body.style.backgroundColor = 'yellow'; break;
+        case 'rosa': body.style.backgroundColor = 'pink'; break;
+        default: body.style.backgroundColor = 'black';
+    }
+}
